43e5ea98573768f27d01ca429c80aba06f772440
MostafaRabie <mostafamohammed27.mm@gmail.com>
2024-08-14 17:19:11 +0300

    Error Handling in Admin

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,26 +1,70 @@
 const connection = require("../database");
 
 exports.getAllUsers = async (req, res) => {
-  const [result] = await connection.query(
-    "SELECT ID,USERNAME,EMAIL,PASSWORD,ROLE FROM USERS",
-  );
-  res.status(200).json(result);
+  try {
+    const [result] = await connection.query(
+      "SELECT ID,USERNAME,EMAIL,PASSWORD,ROLE FROM USERS",
+    );
+    res.status(200).json(result);
+  } catch (err) {
+    console.error("Error fetching users:", err); // Log the error details
+
+    res.status(500).json({
+      status: "error",
+      requestedAt: req.requestedAt,
+      message:
+        "An error occurred while fetching users. Please try again later.",
+    });
+  }
 };
 
 exports.deleteUser = async (req, res) => {
-  const { id } = req.params;
-  await connection.query(`DELETE FROM USERS WHERE ID = ${id};`);
-  res.status(200).json("Done");
+  try {
+    const { id } = req.params;
+    await connection.query(`DELETE FROM USERS WHERE ID = ${id};`);
+    res.status(200).json("Done");
+  } catch (err) {
+    console.error("Error fetching users:", err); // Log the error details
+
+    res.status(500).json({
+      status: "error",
+      requestedAt: req.requestedAt,
+      message:
+        "An error occurred while fetching users. Please try again later.",
+    });
+  }
 };
 
 exports.giveAdminPermission = async (req, res) => {
-  const { id } = req.params;
-  await connection.query(`UPDATE USERS SET ROLE = 'admin' WHERE ID = ${id};`);
-  res.status(200).json("Done");
+  try {
+    const { id } = req.params;
+    await connection.query(`UPDATE USERS SET ROLE = 'admin' WHERE ID = ${id};`);
+    res.status(200).json("Done");
+  } catch (err) {
+    console.error("Error fetching users:", err); // Log the error details
+
+    res.status(500).json({
+      status: "error",
+      requestedAt: req.requestedAt,
+      message:
+        "An error occurred while fetching users. Please try again later.",
+    });
+  }
 };
 
 exports.removeAdminPermission = async (req, res) => {
-  const { id } = req.params;
-  await connection.query(`UPDATE USERS SET ROLE = 'user' WHERE ID = ${id};`);
-  res.status(200).json("Done");
+  try {
+    const { id } = req.params;
+    await connection.query(`UPDATE USERS SET ROLE = 'user' WHERE ID = ${id};`);
+    res.status(200).json("Done");
+  } catch (err) {
+    console.error("Error fetching users:", err); // Log the error details
+
+    res.status(500).json({
+      status: "error",
+      requestedAt: req.requestedAt,
+      message:
+        "An error occurred while fetching users. Please try again later.",
+    });
+  }
 };
